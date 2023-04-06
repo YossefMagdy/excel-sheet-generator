1f4d1ab036c0c5c12f6e8978e4d5a24e164686af
YossefMagdy <y.f.magdy@gmail.com>
2023-04-06 21:45:12 +0200

    Add sweetalert.js

--- script.js
+++ script.js
@@ -6,24 +6,32 @@ tableExists = false
 const generateTable = () => {
     let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
     table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
-    }
     if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
+        for(let i=0; i<rowsNumber; i++){
+            var tableRow = ""
+            for(let j=0; j<columnsNumber; j++){
+                tableRow += `<td contenteditable></td>`
+            }
+            table.innerHTML += tableRow
+        }
+        if(rowsNumber>0 && columnsNumber>0){
+            tableExists = true
+        }
     }
+    else{
+        Swal.fire('you must enter the number of Row and columns to generate the table')
+    }
+   
 }
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
+    if(tableExists){
+        var elt = table
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
     return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
         : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
+    }
+    else{
+        Swal.fire('no generated table to be exported. ')
+    }
 }
